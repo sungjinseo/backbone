447836a0d154607209a01c0d1e3c1a9bf843f445
greatSeo <sjseo85@gmail.com>
2022-12-28 13:25:06 +0900

    pid 생성문 추가

--- src/main/java/dev/greatseo/backbone/BackboneApplication.java
+++ src/main/java/dev/greatseo/backbone/BackboneApplication.java
@@ -2,12 +2,15 @@ package dev.greatseo.backbone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BackboneApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackboneApplication.class, args);
+        SpringApplication app = new SpringApplication(BackboneApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());   // pid 를 작성하는 역할을 하는 클래스 선언
+        app.run(args);
     }
 
 }
